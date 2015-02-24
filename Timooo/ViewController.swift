3bb87cbdbf777fdc230b92caf3de91adad5daa93
backmeupplz <backmeupplz@gmail.com>
2015-02-23 19:15:59 -0800

    Initial Commit

---
+++ Timooo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timooo
+//
+//  Created by Nikita Kolmogorov on 23/02/15.
+//  Copyright (c) 2015 Borodutch Studio LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
