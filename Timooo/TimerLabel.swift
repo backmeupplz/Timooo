6c3c5ac602dcd1f3b0cd5ca5dc18b7d5dad7da75
backmeupplz <backmeupplz@gmail.com>
2015-02-25 14:38:03 -0800

    Добавил локализацию

---
+++ Timooo/TimerLabel.swift
@@ -0,0 +1,18 @@
+//
+//  TimerLabel.swift
+//  Timooo
+//
+//  Created by Nikita Kolmogorov on 24/02/15.
+//  Copyright (c) 2015 Borodutch Studio LLC. All rights reserved.
+//
+
+import Foundation
+
+class TimerLabel: UILabel {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        self.baselineAdjustment = .AlignCenters
+    }
+}
